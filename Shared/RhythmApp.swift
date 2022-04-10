2ddbca3a104127f413ec69d8a74eba570a6c766f
BB <izum_music@icloud.com>
2022-04-10 20:11:24 +0300

    Initial Commit

---
+++ Shared/RhythmApp.swift
@@ -0,0 +1,17 @@
+//
+//  RhythmApp.swift
+//  Shared
+//
+//  Created by Artem Vinogradov on 10.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct RhythmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
